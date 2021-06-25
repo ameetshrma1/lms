bb624be9312881481fd17b0e8535e4d09216b89d
headaashish <aashdahal@gmail.com>
2021-06-25 17:14:53 +0545

    refractor

--- models/member.js
+++ models/member.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
-const memberSchema = mongoose.Schema({
-  fullName : {
-               type:String , required:true,
-             },
- membership:{
-            type:String,required:true,
-            },
- membershipdate:{
-     type:Date,
-     default:Date.now()
- }
-});
-module.exports = mongoose.model('Member',memberSchema);
+const mongoose = require("mongoose");
+const memberSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    membership: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Member", memberSchema);
